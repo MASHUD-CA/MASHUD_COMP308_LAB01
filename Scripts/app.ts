c2818e9ac2917c57ce91ccb7695c55dc6e03b816
MASHUD-CA <user.email "mashudahmed1980@gmail.com>
2020-01-29 15:40:11 -0500

    Added and Converted TS to JS

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -1,11 +1,17 @@
-//IFEE -- Immediately Inviked function Expresion
-(function () {
-    function Start() {
-        var student;
+  //IFEE -- Immediately Inviked function Expresion
+  (function(){
+
+
+    function Start(){
+
+        let student: objects.Student;
+    
         student = new objects.Student(20, "David", "D333333");
         student.saysHello();
         student.studies();
+        
     }
     window.addEventListener("load", Start);
-})();
-//# sourceMappingURL=app.js.map
+  })();
+    
+  
