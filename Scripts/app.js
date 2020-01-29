92909b27f1c71da2b3dd4244e06b8e0e1845802f
unknown <mashudahmed1980@gmail.com>
2020-01-29 08:58:42 -0500

    Initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,29 @@
+"use strict"; //Help debug. detect error. prevent bug.
+
+//Code block repeatable //NO name function 
+//IFEE= Immediate Invoked Function Expression
+//self-executing anonymous function
+(function(){
+    let myFunctionalVariable = 0;
+
+    function Start(){    // OR let Start = function(){
+        let myLocalVariable = 0;
+        console.log(`App Started ... ${myFunctionalVariable}`);
+    }
+
+    window.addEventListener("load", Start);
+
+
+
+
+    //Call back function in line (Annonynmous function)
+    /*window.addEventListener("load", function(){
+        let myLocalVariable = 0;
+        console.log(`App Started ... ${myFunctionalVariable}`);
+    });
+    window.addEventListener("load", () =>{ //Eccma Script-6
+        let myLocalVariable = 0;
+        console.log(`App Started ... ${myFunctionalVariable}`);
+    });
+*/
+})();
